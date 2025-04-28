a0bc3beacf13409b4564f1415ad0ef3400062758
conn-finn <conn.finn@yahoo.com>
2025-04-27 20:28:21 -0400

    finish basic tokenizer

--- Tokenizer.py
+++ Tokenizer.py
@@ -1,6 +1,11 @@
+import re
+import unidecode
+
 class Tokenizer:
     def __init__(self):
         pass
 
-    def tokenize(self):
-        pass
+    def tokenize(line):
+        line = re.sub(r'[^a-zA-Z0-9]', ' ', unidecode.unidecode(line)) # remove punctuation
+        line = line.lower().split()  # lower case
+        return line
